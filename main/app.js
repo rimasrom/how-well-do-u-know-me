bcccc577ebcc010900ba8031560f5de9c5f956c1
rimasrom <hembromsamir715@gmail.com>
2023-03-05 14:09:35 +0530

    Update app.js
    created array and defined function

--- main/app.js
+++ main/app.js
@@ -1,5 +1,41 @@
 const readlineSync = require('readline-sync')
 
-const userAnswer = readlineSync.question("Enter your Name : ")
-console.log(`welcome ${userAnswer}, lets play a quiz...`)
+const userName = readlineSync.question("Enter your Name : ")
+console.log(`welcome ${userName}, lets play a quiz...`)
+
+const commonPart = "what would i choose"
+
+const questions = [
+    {
+        question : `${commonPart}, car or bike ?`,
+        answer : "bike"
+    },
+    {
+        question : `${commonPart}, mountain or beach ?`,
+        answer : "mountain"
+    },
+    {
+        question : `${commonPart}, biryani or pizza ?`,
+        answer : "biryani"
+    }
+]
+
+const quiz = (question,answer)=>{
+    const userAnswer = readlineSync.question(question)
+    if(userAnswer===answer){
+        console.log("you are right")
+    }else{
+        console.log("you are wrong")
+    }
+}
+
+const play = () =>{
+    for(i=0;i<questions.length;i++){
+    const currentQuestion = questions[i]
+    quiz(currentQuestion.question,currentQuestion.answer)
+    }
+}
+
+play();
+
 
